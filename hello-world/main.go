e18748a0098f16530c4ecce806779eff7b817614
Felix <51448701+Felix1862@users.noreply.github.com>
2025-06-22 00:31:35 +0100

    Create main.go

---
+++ hello-world/main.go
@@ -0,0 +1,15 @@
+package main
+
+// This is a single-line comment explaining that we're importing the "fmt" package
+import "fmt"
+
+/*
+This is a multi-line comment
+This Go program prints "Hello World" to the terminal.
+It serves as a basic starter program and checks if the Go setup is working.
+*/
+
+func main() {
+	// Print "Hello World" to the terminal
+	fmt.Println("Hello World")
+}
